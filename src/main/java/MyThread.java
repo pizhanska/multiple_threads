46b9898f77bc7a8a122ee9ec004adec50711fc8e
pizhanska <pizhanska29@gmail.com>
2017-05-21 22:15:19 +0300

    edit adding elements to queue

--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -7,17 +7,23 @@ public class MyThread implements Runnable {
     private ConcurrentLinkedQueue<String> concurrentLinkedQueue;
     private String input;
 
-    public MyThread(ConcurrentLinkedQueue<String> concurrentLinkedQueue, String input){
+    public MyThread(ConcurrentLinkedQueue<String> concurrentLinkedQueue){
         this.concurrentLinkedQueue = concurrentLinkedQueue;
-        this.input = input;
+
     }
 
     public void run() {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 10; i++) {
+                concurrentLinkedQueue.add("Record =" + i + ";");
+                System.out.println("Add to queue: "+Thread.currentThread().getName());
+
         }
-        concurrentLinkedQueue.add(input);
     }
+
 }
+
