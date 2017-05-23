526d3828ce0cc4f3355747305134b7b2eb5fbcfc
pizhanska <pizhanska29@gmail.com>
2017-05-23 20:56:47 +0300

    edit functionality of writing to file

--- src/main/java/FileWriter.java
+++ src/main/java/FileWriter.java
@@ -11,7 +11,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * Created by Uliana Pizhanska on 19/05/2017.
  */
 public class FileWriter {
-    private static  String input[] = {"2","3", "4", "5", "6","1","0"};
 
     public static void main(String[] args) {
         File file=new File("ThreadDemo.txt");
@@ -26,8 +25,8 @@ public class FileWriter {
         try {
             FileOutputStream out=new FileOutputStream(file, true);
             ConcurrentLinkedQueue<String> concurrentLinkedQueue=new ConcurrentLinkedQueue<String>();
-            Thread t1 = new Thread(new MyThread(concurrentLinkedQueue, input));
-            Thread t2 = new Thread(new MyThread(concurrentLinkedQueue,input));
+            Thread t1 = new Thread(new MyThread(concurrentLinkedQueue));
+            Thread t2 = new Thread(new MyThread(concurrentLinkedQueue));
             t1.start();
             t2.start();
             try {
